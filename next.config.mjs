2028cf5b801e5b57f18578b38890f89eff9def1b
raazkhnl <raazkhnl@gmail.com>
2025-01-18 03:26:51 +0545

    Update image configuration to use remote patterns for Unsplash

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'images.unsplash.com',
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
         ],
     },
 };
